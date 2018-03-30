6a157ddaee50b9c53563d51db4af22452bedc510
lanzhiheng <lanzhihengrj@gmail.com>
2018-03-30 11:42:56 +0800

    Simple login flow

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330013632) do
+ActiveRecord::Schema.define(version: 20180330023922) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
